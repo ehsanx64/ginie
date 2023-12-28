22d067bb7495756cbac234ebc3ae33b4ad63a310
Ehsan Mohammadi <ehsanx64@gmail.com>
2023-12-28 18:42:05 +0330

    feat: add basic pages and templates

--- main.go
+++ main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 
+	// Define templates
+	router.LoadHTMLGlob("templates/**/*")
+
 	// Define static routes
-	router.StaticFile("/", "./resources/home.html")
+	router.StaticFile("/landing", "./resources/landing.html")
 	router.Static("/assets", "./assets")
 	router.StaticFS("/static", http.Dir("./static"))
+
+	// Define routes
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "pages/home.tmpl", gin.H{
+			"title": "Home",
+		})
+	})
+
+	router.GET("/about", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "pages/about.tmpl", gin.H{
+			"title": "About",
+		})
+	})
 
 	// Define API endpoints
 	router.GET("/api/", func(c *gin.Context) {
