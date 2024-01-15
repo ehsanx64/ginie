35ae984391af7803592a42f6f64947d01eb46be8
Ehsan Mohammadi <ehsanx64@gmail.com>
2024-01-15 23:02:18 +0330

    refactor: move test code to go-examples repo packages/gin folder

--- api.go
+++ api.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,10 +10,6 @@ func RegisterApiRoutes(router *gin.Engine) {
 	{
 		apiRoute.GET("/", index)
 		apiRoute.GET("/ping", ping)
-
-		apiRoute.GET("/form-data-bind/getb", formDataBind_getb)
-		apiRoute.GET("/form-data-bind/getc", formDataBind_getc)
-		apiRoute.GET("/form-data-bind/getd", formDataBind_getd)
 	}
 }
 
@@ -28,63 +22,5 @@ func index(c *gin.Context) {
 func ping(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "pong",
-	})
-}
-
-type StructA struct {
-	FieldA string `form:"field_a" binding:"required"`
-}
-
-type StructB struct {
-	NestedStruct StructA
-	FieldB       string `form:"field_b" binding:"required"`
-}
-
-type StructC struct {
-	NestedStructPointer *StructA
-	FieldC              string `form:"field_c"`
-}
-
-type StructD struct {
-	NestedAnonStruct struct {
-		FieldX string `form:"field_x"`
-	}
-	FieldD string `form:"field_d"`
-}
-
-func formDataBind_getb(c *gin.Context) {
-	var b StructB
-
-	err := c.Bind(&b)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"a": b.NestedStruct,
-		"b": b.FieldB,
-	})
-}
-
-func formDataBind_getc(c *gin.Context) {
-	var sc StructC
-
-	c.Bind(&sc)
-	c.JSON(http.StatusOK, gin.H{
-		"a": sc.NestedStructPointer,
-		"c": sc.FieldC,
-	})
-}
-
-func formDataBind_getd(c *gin.Context) {
-	var d StructD
-
-	c.Bind(&d)
-	c.JSON(http.StatusOK, gin.H{
-		"x": d.NestedAnonStruct,
-		"d": d.FieldD,
 	})
 }
